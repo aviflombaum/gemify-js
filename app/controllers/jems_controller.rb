a552a4a4f18d757c820b28a6a86109f5105ccdb0
Daniel Yang <yangsunwoo@gmail.com>
2014-04-09 13:29:24 -0400

    added generator for rails/gems

--- app/controllers/jems_controller.rb
+++ app/controllers/jems_controller.rb
@@ -6,6 +6,22 @@ class JemsController < ApplicationController
 
   def create
     @jem = Jem.new(jem_params)
+
+    respond_to do |format|
+      if @jem.save
+        begin
+          `RAILS_ENV=#{Rails.env.to_s} bundle exec rails generate gemify #{@jem.id}`
+        rescue Exception => e
+          logger.info "#{e.inspect}"
+        end
+
+        format.html { redirect_to @jem, notice: 'Jem was successfully created.' }
+        format.json { render json: @jem, status: :created, location: @jem }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @jem.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def index
---
+++ lib/generators/gemify/gemify_generator.rb
@@ -0,0 +1,35 @@
+class GemifyGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+
+  def source_paths
+    [File.expand_path('../templates', __FILE__), Dir.pwd]
+  end
+
+  def generate_files
+    target = File.join(Dir.pwd, "jems/#{jem.name}")
+    template "engine.rb.erb", File.join(target, "lib/#{jem.name}/engine.rb")
+    template "version.rb.erb", File.join(target, "lib/#{jem.name}/version.rb")
+    template "railtie.rb.erb", File.join(target, "lib/#{jem.name}/railtie.rb")
+    template "railsloader.rb.erb", File.join(target, "lib/#{jem.name}.rb")
+    template "gemspec.rb.erb", File.join(target, "#{jem.name}.gemspec")
+    template "README.md.tt", File.join(target, "README.md")
+
+    repo = GithubService.create_repository(jem.name)
+    GithubService.add_collaborator(repo.full_name, jem.creator.login)
+    GithubService.push_local_repo("jems/#{jem.name}", repo.ssh_url)
+
+    Dir.chdir(target) do
+      version = "0.0.1"
+      RubyGemService.create_gem(jem.name, "#{target}/#{jem.name}-#{version}.gem")
+    end
+  end
+
+  private
+  def name_for_class
+    jem.base_name.capitalize
+  end
+
+  def jem
+    Jem.find(name.to_i)
+  end
+end
