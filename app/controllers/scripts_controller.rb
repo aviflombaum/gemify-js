744cbc1941439ca1c9979a0ac8b136e37282b180
wontaeyang <wontaeyang@gmail.com>
2014-04-15 15:32:25 -0400

    added ajax activity monitor

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -3,7 +3,7 @@ class ScriptsController < ApplicationController
   def create
     @script = Jem.find(params[:id]).scripts.create(file: params[:script][:file])
     @jem = Jem.find(params[:id])
-    track_activity(@jem, @script, current_user)
+    @activity = track_activity(@jem, @script, current_user)
 
     respond_to do |format|
       format.js
@@ -13,7 +13,7 @@ class ScriptsController < ApplicationController
   def destroy
     @script = Script.find(params[:id])
     @jem = Jem.find(@script.jem_id)
-    track_activity(@jem, @script, current_user)
+    @activity = track_activity(@jem, @script, current_user)
     @script.destroy
     respond_to do |format|
       format.js
